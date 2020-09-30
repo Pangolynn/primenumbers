a0181b23147f862d4f90c7e6b04ca41450eb698c
JAI-2002 <72140256+JAI-2002@users.noreply.github.com>
2020-10-01 00:14:37 +0530

    The new code is compact and better as it is easily understood

--- primenumbers.py
+++ primenumbers.py
@@ -1,42 +1,60 @@
-#Samantha Holloway 10/2014
-#Editted 11/05/2014 Joseph Pannizzo
-#OOD
-#Program will output all prime numbers up to a given input
+# #Samantha Holloway 10/2014
+# #Editted 11/05/2014 Joseph Pannizzo
+# #OOD
+# #Program will output all prime numbers up to a given input
 
 
-#-----Function Definitions----
+# #-----Function Definitions----
 
-#Function finds out if a number is prime and prints it
-def prime(number):
-        i=1
-        #any factors must be less than the square root
-        #of the number, +1 for python not including upper limit
-        upper=int((number ** 0.5))+1
-        for i in range(2,upper):
-            if number % i == 0: #composite -- do nothing
-                break
-        else:
-            print(number) #prime
-
-
-#Function loops through all numbers between 1 and the given number
-#Calls prime() on each number
-def primeloop(number):
-    if number > 1:
-        for j in range(2, number+1):
-            prime(j)
-    else:
-        print("1 is not a prime number, and has no primes before it")
+# #Function finds out if a number is prime and prints it
+# def prime(number):
+#         i=1
+#         #any factors must be less than the square root
+#         #of the number, +1 for python not including upper limit
+#         upper=int((number ** 0.5))+1
+#         for i in range(2,upper):
+#             if number % i == 0: #composite -- do nothing
+#                 break
+#         else:
+#             print(number) #prime
 
 
-#----------Program Start--------------
-#Added exception handling. If input isn't a number program will loop.
-while True:
-	try:
-		#number is user input
-		number = int(input("Enter a number: "))
-		break
-	except Exception:
-		print("That's not a valid number. Please use a whole number.")
+# #Function loops through all numbers between 1 and the given number
+# #Calls prime() on each number
+# def primeloop(number):
+#     if number > 1:
+#         for j in range(2, number+1):
+#             prime(j)
+#     else:
+#         print("1 is not a prime number, and has no primes before it")
+
+
+# #----------Program Start--------------
+# #Added exception handling. If input isn't a number program will loop.
+# while True:
+# 	try:
+# 		#number is user input
+# 		number = int(input("Enter a number: "))
+# 		break
+# 	except Exception:
+# 		print("That's not a valid number. Please use a whole number.")
 		
-primeloop(number)
+# primeloop(number)
+
+
+
+
+#much better code
+n = int(input("Enter a number: "))
+k = 2
+
+while k <= n:
+    d = 2
+    flag = False
+    while d < k:
+        if k%d == 0:
+            flag = True
+        d = d + 1
+    if flag == False:
+        print(k)
+    k = k + 1
